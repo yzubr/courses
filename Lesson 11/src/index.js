926f5bc38d39799b69b183853299ac0e28157cbd
yzubr <vadzim.vinnikau@gmail.com>
2024-10-07 20:11:34 +0300

    Fix lesson 11

--- Lesson 11/src/index.js
+++ Lesson 11/src/index.js
@@ -1,40 +1,43 @@
 // string length
 
-const s = 'agdjgfkshv'
-console.log(s.length)
+const string10 = 'agdjgfkshv'
+
+console.log(string10.length)
 
 // string reverce
 
-function reverseStr(a) {
-  return a.split('').reverse().join('')
+function reverseStr(string) {
+  return string.split('').reverse().join('')
 }
 
-console.log(reverseStr(s))
+console.log(reverseStr(string10))
 
 // substr
 
-function findSubStr(str, substr) {
-  if (str.indexOf(substr) !== -1) {
-    console.log(`в строке ${str} есть подстрока ${substr}`)
-  } else console.log(`в строке ${str} нет подстроки ${substr}`)
+function findSubStr(string, substring) {
+  if (string.indexOf(substring) !== -1) {
+    console.log(true)
+  } else {
+    console.log(false)
+  }
 }
 
 findSubStr('мама мыла раму', 'ла')
 
 // array
 
-const arr = ['str', 15]
-console.log(arr.length)
-arr.push('str1', {})
-arr.unshift(10, [])
-console.log(arr.length)
-arr.splice(2)
-console.log(arr)
+const array = ['str', 15]
+console.log(array.length)
+array.push('str1', {})
+array.unshift(10, [])
+console.log(array.length)
+array.splice(2)
+console.log(array)
 
 // fuction array-reverse
 
-function reverceArray(arr1) {
-  console.log(arr1.reverse())
+function reverceArray(array1) {
+  console.log(array1.reverse())
 }
 
 reverceArray([1, 2, 3, 4, 5])
