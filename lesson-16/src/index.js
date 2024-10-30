fc5aaef317c9e954129f20f90b94d7afd8f3a85b
yzubr <vadzim.vinnikau@gmail.com>
2024-10-30 21:12:03 +0300

    Add fix all bugs

--- lesson-16/src/index.js
+++ lesson-16/src/index.js
@@ -5,12 +5,14 @@ console.log(document.querySelectorAll('article')[0].firstElementChild)
 
 changeContent.addEventListener('click', () => {
   document.querySelector('.content-for-changing').innerHTML = `
- <p>Я содержимое которое <span>изменили</span></p>`
+ <p>Я содержимое которое <span>изменили</span></p>
+ `
 })
 
 changeElement.addEventListener('click', () => {
   document.querySelector('#element-for-change').outerHTML = `
- <div>Я div<div>`
+ <div>Я div<div>
+ `
 })
 
 // 3
@@ -31,11 +33,11 @@ document.querySelector('#form-4').addEventListener('submit', (event) => {
 const resultOfFindingProducts = document.querySelector('.result-of-finding-products')
 document.querySelector('.find-products').addEventListener('submit', async (event) => {
   event.preventDefault()
-  console.log(document.querySelector('#find-product-input').value)
-  const response = await fetch(`https://dummyjson.com/products/search?q=${document.querySelector('#find-product-input').value}`)
+  const valueOfInput = document.querySelector('#find-product-input').value
+  const response = await fetch(`https://dummyjson.com/products/search?q=${valueOfInput}`)
   const result = await response.json()
   result.products.forEach((product) => {
-    const productHTML = document.createElement('p')
+    const productHTML = document.createElement('li')
     productHTML.innerText = `наименование товара: ${product.title}`
     resultOfFindingProducts.appendChild(productHTML)
   })
