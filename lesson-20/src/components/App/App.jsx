aa59c40a9a7fc1860e744ebdb29f4f0e4a0c08e8
yzubr <vadzim.vinnikau@gmail.com>
2024-11-17 21:13:07 +0300

    Add lesson-20

--- lesson-20/src/components/App/App.jsx
+++ lesson-20/src/components/App/App.jsx
@@ -1,11 +1,29 @@
-import products from '/src/data/products.js'
-import CardRendering from '../'
+import CardItem from '../CardRendering/CardRendering.jsx'
+import Greeting from '../Greeting/Greeting.jsx'
+import UserStatus from '../UserStatus/UserStatus.jsx'
 import styles from './App.module.css'
+import products from '/src/data/products.js'
 
 export default function App() {
   return (
-    <section className={styles.section}>
-      products.map((product) => <CardRendering />)
-    </section>
+    <>
+      <div className={styles.separator}>
+        <p>Task 1</p>
+      </div>
+      <Greeting name = {'Robert'}/>
+      <div className={styles.separator}>
+        <p>Task 2</p>
+      </div>
+      <UserStatus isOnline = {true}/>
+      <div className={styles.separator}>
+        <p>Task 3</p>
+      </div>
+      <h1>List of products</h1>
+      <ul>
+        {products.map((product) => (
+          <CardItem key={product.id} product = {product}/>
+        ))}
+      </ul>
+    </>
   )
 }
