ff6f601dd127a2f06d9917171b6f76ed22789337
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-11-23 00:01:01 +0300

    Simplify footer styles, fix link

--- lesson-19/src/components/footer/Footer.jsx
+++ lesson-19/src/components/footer/Footer.jsx
@@ -1,10 +1,9 @@
 import styles from './Footer.module.css'
-import headerStyles from '../header/Header.module.css'
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>2023. Разработано компанией <a href="#" className={headerStyles['navigation-item']}>«Гарцующий пони»</a></p>
+      <p>2023. Разработано компанией <a href="/">«Гарцующий пони»</a></p>
     </footer>
   )
 }
