2dbb2e9a6aaa7466ed47c078002d2e4e7f00d4cb
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-11-26 10:14:34 +0300

    Fix style files order

--- lesson-24/src/main.jsx
+++ lesson-24/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import 'the-new-css-reset'
-import App from './components/App.jsx'
 import './styles.css'
+import App from './components/App.jsx'
 
 const root = createRoot(document.getElementById('app'))
 
